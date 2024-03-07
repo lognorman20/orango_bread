88046d311dafb703d6d645487f31c6e161b9cc1e
Logan Norman <lognorman20@gmail.com>
2024-03-07 14:24:26 +1300

    basic working

---
+++ src/ProductCard.tsx
@@ -0,0 +1,41 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+export default function ProductCard({
+  productName,
+  productDescription,
+  imgRef,
+  link
+}: {
+  productName: string;
+  productDescription: string;
+  imgRef: string;
+  link: string;
+}) {
+  return (
+    <Link href={link} style={{ textDecoration: 'none' }}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={imgRef}
+            alt={productName}
+          />
+          <CardContent style={{ backgroundColor: '#fff59d', padding: '8px' }}>
+            <Typography gutterBottom variant="h5" component="div">
+              {productName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {productDescription}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
+  );
+}
---
+++ src/ProductList.tsx
@@ -0,0 +1,29 @@
+import Grid from '@mui/material/Grid';
+import ProductCard from './ProductCard';
+
+const products = [
+  {
+    productName: "Loaf",
+    productDescription: "A loaf of our signature banana bread.",
+    imgRef: "loaf.jpeg",
+    link: "https://buy.stripe.com/test_4gwbLcfyLe9G17GfZ0"
+  },
+  {
+    productName: "Slice",
+    productDescription: "A singular slice of our banana bread.",
+    imgRef: "slice.jpeg",
+    link: "https://buy.stripe.com/test_14k8z086jc1y17G9AB"
+  }
+];
+
+export default function ProductList() {
+    return (
+        <Grid container direction="column" spacing={3}>
+            {products.map((product, index) => (
+                <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+                    <ProductCard {...product} />
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
